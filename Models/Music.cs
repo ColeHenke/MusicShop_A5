558a9e72eafbd976ea17563ca0530c1cf11e689c
Cole Henke <cole.henke@ndus.edu>
2023-12-01 16:57:04 -0600

    Add project files.

---
+++ Models/Music.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicShop.Models
+{
+    public class Music
+    {
+        [Key]
+        public required int SongId { get; set; }
+        public required string SongName { get; set; }
+        public required string ArtistName { get; set; }
+        public required string Genre { get; set; }
+        public required decimal Price { get; set; }
+
+    }
+}
