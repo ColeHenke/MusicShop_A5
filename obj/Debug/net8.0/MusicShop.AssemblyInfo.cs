ea3bd9683db5694040d28a1b97634853619715e2
Cole Henke <cole.henke@ndus.edu>
2023-12-01 16:59:20 -0600

    Extra

--- obj/Debug/net8.0/MusicShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caec16ce92bca5ecabc17b902e7f5a9ca442ee92")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
